6a618c814e6aea2dfd134d39b4d4f1aeabc8fe9c
vinimateuslima <viniciusmateus.dev@gmail.com>
2024-04-12 15:46:59 -0300

    criando a seção de instagram

---
+++ src/components/Instagram.jsx
@@ -0,0 +1,21 @@
+import "./Instagram.css";
+
+const Instagram = () => {
+  return (
+    <section className="instagram">
+      <h2>Instagram</h2>
+      <div className="instagram_grid">
+        <div className="caixa"></div>
+        <div className="caixa"></div>
+        <div className="caixa"></div>
+        <div className="caixa"></div>
+        <div className="caixa"></div>
+        <div className="caixa"></div>
+        <div className="caixa"></div>
+        <div className="caixa"></div>
+      </div>
+    </section>
+  );
+};
+
+export default Instagram;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,12 +4,15 @@ import Servicos from "../components/Servicos";
 
 import Marcar from "../components/Marcar";
 
+import Instagram from "../components/Instagram";
+
 const Home = () => {
   return (
     <>
       <Banner />
       <Servicos />
       <Marcar/>
+      <Instagram/>
     </>
   );
 };
