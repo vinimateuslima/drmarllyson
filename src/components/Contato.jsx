51d6f89b270e4912bce8942ff4fdadbfb1ae8ef0
vinimateuslima <viniciusmateus.dev@gmail.com>
2024-04-13 13:35:07 -0300

    iniciando tela de contato

---
+++ src/components/Contato.jsx
@@ -0,0 +1,17 @@
+import { Row, Col, Button } from "reactstrap";
+
+const Contato = () => {
+  return (
+    <section className="contato">
+        <Row>
+            <Col lg="6">
+                <h2>Entre em contato conosco!</h2>
+            </Col>
+            <Col lg="6">
+            </Col>
+        </Row>
+    </section>
+  )
+}
+
+export default Contato
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,13 +6,20 @@ import Marcar from "../components/Marcar";
 
 import Instagram from "../components/Instagram";
 
+import Contato from "../components/Contato";
+
+import "./Home.css"
+
 const Home = () => {
   return (
     <>
       <Banner />
+      <main className="principal">
       <Servicos />
       <Marcar/>
       <Instagram/>
+      <Contato/>
+      </main>
     </>
   );
 };
